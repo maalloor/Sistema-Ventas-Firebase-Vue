70b02cd932192b0b2f2ef196c2254ad62e776d8a
maalloor <maalloor@espol.edu.ec>
2021-12-05 18:10:18 -0500

    Crear componentes para listar y crear electronicos

---
+++ src/components/CreateElectronic.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="card card-body mt-4">
+        <form @submit.prevent="onSubmit">
+            <div class="form-group">
+                <label>Reviewer ID</label>
+                <input v-model="form.reviewerID" class="form-control" required />
+            </div>
+
+            <div class="form-group">
+                <label>Reviewer Name</label>
+                <input v-model="form.reviewerName" class="form-control" required />
+            </div>
+
+            <div class="form-group">
+                <label>Overall</label>
+                <input v-model="form.overall" class="form-control" type="number" required />
+            </div>
+
+            <button type="submit" class="btn btn-success mt-3">
+                Create Electronic
+            </button>
+        </form>
+    </div>
+</template>
+
+<script>
+import { createElectronic } from '@/firebase'
+import { reactive } from 'vue'
+export default {
+    setup()
+    {
+        const form = reactive({ reviewerID: '', reviewerName: '', overall: 0 })
+        const onSubmit = async () => {
+            await createElectronic({ ...form })
+            form.reviewerID = ''
+            form.reviewerName = ''
+            form.overall = 0
+        }
+        return { form, onSubmit }
+    }
+}
+</script>
---
+++ src/components/ListElectronics.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="card mt-4">
+        <table class="table m-0">
+            <thead>
+                <tr>
+                <th scope="col">Reviewer ID</th>
+                <th scope="col">Reviewer Name</th>
+                <th scope="col">Overall</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="{ id, reviewerID, reviewerName, overall } in electronics" :key="id">
+                    <td>{{ reviewerID }}</td>
+                    <td>{{ reviewerName }}</td>
+                    <td>{{ overall }}</td>
+                    <td>
+                        <router-link :to="`/edit/${id}`">
+                            <button class="btn btn-primary btn-sm me-2">
+                                Edit
+                            </button>
+                        </router-link>
+                        <button class="btn btn-danger btn-sm" @click="deleteUser(id)">
+                            Delete
+                        </button>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+import { useLoadElectronics, deleteElectronic } from '@/firebase'
+export default {
+    setup()
+    {
+        const electronics = useLoadElectronics()
+        return { electronics, deleteElectronic }
+    }
+}
+</script>
